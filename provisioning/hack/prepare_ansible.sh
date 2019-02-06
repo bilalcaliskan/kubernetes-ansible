4105d343c116bf2aa900d69aa3aba87a0d018b7f
= <bilalccaliskan@gmail.com>
2019-02-06 23:01:08 +0300

    assume yes on install commands

--- provisioning/hack/prepare_ansible.sh
+++ provisioning/hack/prepare_ansible.sh
@@ -7,12 +7,12 @@ then
 	if [[ -n "$(sudo which yum)" && ! -n "$(sudo which easy_install)" ]]
 	then
 		echo "package manager of OS is yum and easy_install is not installed, installing..."
-		sudo yum install python-setuptools
+		sudo yum install -y python-setuptools
 	elif [[ -n "$(which apt)" && ! -n "$(sudo which easy_install)" ]]
 	then
 		echo "package manager of OS is apt and easy_install is not installed, installing..."
 		sudo apt update
-		sudo apt install python-setuptools
+		sudo apt install -y python-setuptools
 	fi
 
 	sudo easy_install pip
